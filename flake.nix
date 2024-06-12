b014ba403f3aee621a2c6455ada55f433f7eac8f
Simcra <simcra@live.com>
2024-06-12 22:36:17 +0930

    Create template slint application, fix broken flake.nix

--- flake.nix
+++ flake.nix
@@ -14,14 +14,21 @@
       pkgs = import nixpkgs {
         system = "x86_64-linux";
         overlays = [ (import rust-overlay) ];
-      }; 
+      };
     in
     {
       packages.x86_64-linux.default = pkgs.callPackage ./. { };
       devShells.x86_64-linux.default = pkgs.mkShell {
         buildInputs = with pkgs; [
+          openssl
+          pkg-config
           rust-bin.stable.latest.default
         ];
+        LD_LIBRARY_PATH = nixpkgs.lib.makeLibraryPath (with pkgs; [
+          wayland
+          libxkbcommon
+          fontconfig
+        ]);
       };
     };
 }
